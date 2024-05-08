11a8b0a6ab3e88de61322a113b6a43d4bcf91e75
paolo flore <paoloflore937@gmail.com>
2024-05-08 14:05:41 +0200

    classe Videogame

---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    internal class Videogame
+    {
+
+
+        public static class VideogameManager
+        {
+
+        }
+    }
+
+}
